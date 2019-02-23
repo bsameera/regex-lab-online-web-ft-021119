b5dca478f00c9463f5113c9270a9fe793029627a
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-23 04:05:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  output = /\A[A-Z]\w\./.match(word)
+  return output==nil ? false : true
 end
 
 def valid_phone_number?(phone)
