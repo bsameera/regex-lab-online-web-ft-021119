14f3cfe430133973e478ccc7f029bb2fab865f89
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-23 03:35:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 def starts_with_a_vowel?(word)
   output = /\A[aeiouAEIOU]\w/.match?(word)
-  output
+  return output==nil ? false : true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
